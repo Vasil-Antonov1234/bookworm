05a06eb6fcda057132a59a576f2b56497a458127
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-06-25 22:53:57 +0300

    Consolidate book search into single route

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -11,9 +11,9 @@ bookController.post("/create", async (req, res) => {
     const newBook = req.body;
 
     try {
-       await bookService.create(newBook);
-       
-       res.redirect("/");
+        await bookService.create(newBook);
+
+        res.redirect("/");
     } catch (error) {
         console.log(error.message);
     };
@@ -23,48 +23,38 @@ bookController.get("/:bookId/details", async (req, res) => {
     const bookId = req.params.bookId;
 
     try {
-      const book = await bookService.getById(bookId);
-      
-      res.render("books/details", book);
+        const book = await bookService.getById(bookId);
+
+        res.render("books/details", book);
     } catch (error) {
         console.log(error.message);
     };
 });
 
 bookController.get("/search", async (req, res) => {
-    
-    try {
-        const books = await bookService.getAll();
-        
-        res.render("books/search", { books })
-    } catch (error) {
-        console.log(error)
-    };
-});
 
-bookController.get("/search/book", async (req, res) => {
     const searchData = req.query;
-    
+
     try {
         const allBooks = await bookService.getAll();
         let filteredBooks = allBooks;
 
-        if (searchData.year) {
+        if (searchData && searchData.year) {
             filteredBooks = allBooks.filter((x) => x.year.includes(searchData.year));
         }
 
-        if (searchData.title) {
+        if (searchData && searchData.title) {
             filteredBooks = filteredBooks.filter((x) => x.title.toLowerCase().includes(searchData.title.toLocaleLowerCase()));
         }
 
-        if (searchData.genre) {
+        if (searchData && searchData.genre) {
             filteredBooks = filteredBooks.filter((x) => x.genre.toLocaleLowerCase().includes(searchData.genre.toLocaleLowerCase()));
         }
-        
+
         res.render("books/search", { books: filteredBooks });
     } catch (error) {
         console.log(error.message)
     }
-})
+});
 
 export default bookController;
