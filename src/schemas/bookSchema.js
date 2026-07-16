c5f4efe099aef2228c967bd41f142f9291556570
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-07-16 22:07:15 +0300

    Update bookSchema.js
    Add enum validation for category

--- src/schemas/bookSchema.js
+++ src/schemas/bookSchema.js
@@ -4,6 +4,7 @@ export const createBookSchema = z.object({
     title: z.string()
         .min(5, { message: "Title must be at least 5 characters long" })
         .regex(/^[A-Za-z0-9 ]+$/, { message: "Title can only contains letters, numbers and spaces" }),
+    category: z.enum(["novel", "poetry", "biography", "non-fiction", "short-story"], { message: "Please select a category from the list" }),
     genre: z.string()
         .min(5, { message: "Genre must be at least 5 characters long" })
         .regex(/^[A-Za-z0-9 ]+$/, { message: "Genre can only contains letters, numbers and spaces" }),
@@ -11,14 +12,14 @@ export const createBookSchema = z.object({
         .min(5, { message: "Author name must be at least 5 characters long" })
         .regex(/^[A-Za-z0-9 ]+$/, { message: "Author name can only contains letters, numbers and spaces" }),
     year: z.coerce.number()
-        .min(1900, { message: "Year must be greater than or equal to 1900"})
-        .max(new Date().getFullYear(), { message: `Year must be less than or equal to ${new Date().getFullYear()}`}),
+        .min(1900, { message: "Year must be greater than or equal to 1900" })
+        .max(new Date().getFullYear(), { message: `Year must be less than or equal to ${new Date().getFullYear()}` }),
     imageUrl: z.string()
-        .regex(/^https?:\/\//, { message: "Image URL must starts with http:// or https://"}),
+        .regex(/^https?:\/\//, { message: "Image URL must starts with http:// or https://" }),
     rating: z.coerce.number()
         .min(0, { message: "Rating must be at least 0" })
-        .max(10, { message: "Rating must be at most 10"}),
+        .max(10, { message: "Rating must be at most 10" }),
     description: z.string()
-        .min(20, {message: "Description must be at least 20 characters long."})
-        .regex(/^[A-Za-z0-9 ]+$/, { message: "Description can only contains letters, numbers and spaces" })
+        .min(20, { message: "Description must be at least 20 characters long." })
+        .regex(/^[A-Za-z0-9 ]+$/, { message: "Description can only contains letters, numbers and spaces" }),
 })
