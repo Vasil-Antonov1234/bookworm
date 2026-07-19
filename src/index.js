f18885d9ef9507bab211d54b9ce670757baed239
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-07-19 21:46:59 +0300

    Add extract view helpers

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import { engine } from "express-handlebars";
 import routes from "./routes.js";
 import { authMiddleware } from "./middlewares/authMiddleware.js";
 import cookieParser from "cookie-parser";
+import * as helpers from "./views/helpers";
 
 const app = express();
 
 app.engine("hbs", engine({
     extname: "hbs",
-    helpers: {
-        setTitle(title) {
-            this.pageTitle = title;
-        }
-    }
+    helpers
 }));
 app.set("view engine", "hbs");
 app.set("views", "./src/views");
